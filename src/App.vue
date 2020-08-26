6f754c24159a15f50c839448b49052e71d85cc3b
Leszek Kicior <leszek.kicior@fluentbe.com>
2020-08-26 02:36:45 +0200

    Font changed to Nunito Sans

--- src/App.vue
+++ src/App.vue
@@ -16,8 +16,11 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;1,300&display=swap');
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Nunito Sans', Helvetica, Arial, sans-serif;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
