a4db2dd3866c4fa62da73c7a76adada8f92c0d53
Leszek Kicior <leszek.kicior@fluentbe.com>
2020-08-31 00:58:54 +0200

    Vue-confetti added to main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueConfetti from 'vue-confetti';
 
-require('@/assets/styles/style.scss')
+Vue.use(VueConfetti);
+
+require('@/assets/styles/style.scss');
 
 Vue.config.productionTip = false
 
